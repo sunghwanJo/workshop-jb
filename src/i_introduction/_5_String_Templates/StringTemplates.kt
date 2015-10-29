d2bdefef70070cf55b965dad38bc670843e2d194
JoSunghwan <sunghwanjo94@gmail.com>
2015-10-29 19:37:06 +0900

    [KOTLIN][5_String_Templates] 문자열안에 변수를 넣을때는 $를 붙이면된다.
    정규표현식도 덤으로 학습.
    
    Author: SunghwanJo <sunghwanjo94@gmail.com>

--- src/i_introduction/_5_String_Templates/StringTemplates.kt
+++ src/i_introduction/_5_String_Templates/StringTemplates.kt
@@ -3,7 +3,7 @@ package i_introduction._5_String_Templates
 import java.util.regex.Pattern
 import util.*
 
-fun getPattern() = """(\w*) (\w*) \((\d{2})\.(\d{2})\.(\d{4})\)"""
+fun getPattern() = """(\w*) (\w*) \((\d{2}) $month (\d{4})\)"""
 
 fun example() = Pattern.compile(getPattern()).matcher("Douglas Adams (11.03.1952)").find()
 
@@ -19,4 +19,4 @@ fun todoTask4(): Nothing = TODO(
     documentation = doc4(),
     references = { getPattern(); month })
 
-fun task4(): String = todoTask4()
+fun task4(): String = getPattern()

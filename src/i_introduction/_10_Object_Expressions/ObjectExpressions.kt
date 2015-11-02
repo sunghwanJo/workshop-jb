fb7f898f34f0ef2a0deb60ed3e652094aad8cbb9
JoSunghwan <sunghwanjo94@gmail.com>
2015-11-02 17:17:25 +0900

    [KOTLIN][10_Object_Expressions] Java의 Collections.sort에 필요한 Comparator를 object로 구현하기
    * Listener 구현할때 용이할듯
    * class에 생성자가 있으면 생성자의 값도 전달해야함
    * 상속안받고 그냥 만들 수 있음
    * enclosing scope
    * object 뒤에 이름을 붙이면 접근 가능한 Object이다. 대입이 아니라 접근임.
    * object는 local에 정의될 수 없음. function 안에 직접적으로 포함되어야함(?!)
    * 다른 object의 정의나 inner 클래스가 아닌 곳에 포함될 수 있다.
    
    Author: SunghwanJo <sunghwanjo94@gmail.com>

--- src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -17,6 +17,14 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int>{
+        override fun compare(o1: Int, o2: Int): Int {
+            if(o1 < o2) return 1
+            else return if(o1 == o2) 0
+            else return -1
+        }
+    })
+
     return arrayList
 }
+

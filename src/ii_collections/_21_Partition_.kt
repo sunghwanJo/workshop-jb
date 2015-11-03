9d7979b48354475fa7c411565fb2c1c0b9747b5c
JoSunghwan <sunghwanjo94@gmail.com>
2015-11-03 14:50:50 +0900

    [KOLTIN][21_Partition] Partition을 사용.
    * partition을 사용할경우 람다식에 의해 나온 Boolean 값을 기준으로 Pair<List, List>를 반환한다.
    
    Author : SunghwanJo <sunghwanjo94@gmail.com>

--- src/ii_collections/_21_Partition_.kt
+++ src/ii_collections/_21_Partition_.kt
@@ -12,5 +12,8 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return customers.filter {
+        val (deliveredCustomer, undeliveredCustomer) = it.orders.partition { it.isDelivered }
+        undeliveredCustomer.size > deliveredCustomer.size
+    }.toSet()
 }

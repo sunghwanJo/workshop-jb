a24aa6ba6113373999cdfdc42a17c2211111b42f
JoSunghwan <sunghwanjo94@gmail.com>
2015-11-03 16:04:13 +0900

    [KOTLIN][22_Fold] fold를 사용.
    * Accumulates value starting with initial value and applying operation from left to right to current accumulator value and each element.
    
    Author : SunghwanJo <sunghwanjo94@gmail.com>

--- src/ii_collections/_22_Fold_.kt
+++ src/ii_collections/_22_Fold_.kt
@@ -15,7 +15,6 @@ fun whatFoldDoes(): Int {
 fun Shop.getProductsOrderedByAllCustomers(): Set<Product> {
     // Return the set of products ordered by every customer
     return customers.fold(allOrderedProducts, {
-        orderedByAll, customer ->
-        todoCollectionTask()
+        orderedByAll, customer -> orderedByAll.intersect(customer.orderedProducts)
     })
 }
